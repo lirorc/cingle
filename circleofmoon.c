659babc6d37ee91963f2e0c0e5e2f4cbdd21c67a
lirorc <74657909+lirorc@users.noreply.github.com>
2022-08-11 23:29:55 +0300

    add first jingle

---
+++ circleofmoon.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// see etude in c minor
+int main()
+{
+  while (1) {
+    for (int t=0, osc=0; t<8000; t++, osc+=14) {
+      putchar(osc&0x80);
+    }
+    for (int t=0, osc=0; t<4000; t++, osc+=14) {
+      putchar(osc&0x80);
+    }
+    for (int t=0, osc=0; t<4000; t++, osc+=14) {
+      putchar(osc&0x80);
+    }
+    for (int t=0, osc=0; t<8000; t++, osc+=14) {
+      putchar(osc&0x80);
+    }
+    for (int t=0, osc=0; t<4000; t++, osc+=14) {
+      putchar(osc&0x80);
+    }
+    for (int t=0, osc=0; t<4000; t++, osc+=14) {
+      putchar(osc&0x80);
+    }
+    for (int t=0, osc=0; t<8000; t++, osc+=21) {
+      putchar(osc&0x80);
+    }
+    for (int t=0, osc=0; t<2000; t++, osc+=21) {
+      putchar(osc&0x80);
+    }
+    for (int t=0, osc=0; t<2000; t++, osc+=21) {
+      putchar(osc&0x80);
+    }
+    for (int t=0, osc=0; t<4000; t++, osc+=7) {
+      putchar(osc&0x80);
+    }
+    for (int t=0, osc=0; t<8000; t++, osc+=18) {
+      putchar(osc&0x80);
+    }
+    for (int t=0, osc=0; t<2000; t++, osc+=18) {
+      putchar(osc&0x80);
+    }
+    for (int t=0, osc=0; t<2000; t++, osc+=18) {
+      putchar(osc&0x80);
+    }
+    for (int t=0, osc=0; t<4000; t++, osc+=6) {
+      putchar(osc&0x80);
+    }
+  }
+}
